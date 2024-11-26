818dbe6fabf0f72b97f358c9ba12e15ee60b0163
Pavel <dpd2003@gmail.com>
2024-11-26 16:02:58 +0300

    Задача "Модели SQLAlchemy"

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from routers import user, task
 
 app = FastAPI()
 
+app.include_router(user.router)
+app.include_router(task.router)
+
 @app.get('/')
 async def welcome():
     return {'message': 'Welcome to Taskmanager'}
-
-app.include_router(user.router)
-app.include_router(task.router)
